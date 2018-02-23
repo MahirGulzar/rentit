a5ceaf5f20e8d7d156725282363c40c43db67564
ankit <ankit.garg2802@gmail.com>
2018-02-23 03:21:22 +0200

    changes to plantint

--- src/main/java/com/example/demo/models/PlantInventoryItem.java
+++ src/main/java/com/example/demo/models/PlantInventoryItem.java
@@ -4,11 +4,16 @@ package com.example.demo.models;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 
 @Entity
 @Data
 public class PlantInventoryItem {
     String name;
     String description;
+
+    @ManyToOne
+    PlantInventoryEntry plant_info;
+
 
 }
