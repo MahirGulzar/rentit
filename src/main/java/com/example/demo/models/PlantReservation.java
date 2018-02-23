75eadeca7e5dcfa4896e89c4f32d6aa7266c5a92
mushah <mushah@ttu.ee>
2018-02-23 02:43:46 +0200

    adding purchaseOrder and plantReservation classes

---
+++ src/main/java/com/example/demo/models/PlantReservation.java
@@ -0,0 +1,20 @@
+package com.example.demo.models;
+
+import com.example.demo.models.objectvalue.BusinessPeriod;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+public class PlantReservation {
+    @Id
+    @GeneratedValue
+    Long id;
+
+    @Embedded
+    BusinessPeriod schedule;
+
+    //@ManyToOne
+    //PlantInventoryItem plant;
+}
