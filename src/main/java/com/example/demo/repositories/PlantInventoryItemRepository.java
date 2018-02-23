ebed8f636e485c59c975c7de2cf2ef44aba8be46
Junaid Ahmed <junahmed46@gmail.com>
2018-02-23 05:21:49 +0200

    no message

--- src/main/java/com/example/demo/repositories/PlantInventoryItemRepository.java
+++ src/main/java/com/example/demo/repositories/PlantInventoryItemRepository.java
@@ -5,5 +5,6 @@ import com.example.demo.models.PlantInventoryItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface PlantInventoryItemRepository extends JpaRepository<PlantInventoryItem, Long> {}
