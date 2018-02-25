3fa7b2fc3e615893c29d4415949738eaab48a819
mushah <mushah@ttu.ee>
2018-02-25 22:13:12 +0200

    Query 4 initial work...

--- src/main/java/com/example/demo/repositories/PlantInventoryItemRepository.java
+++ src/main/java/com/example/demo/repositories/PlantInventoryItemRepository.java
@@ -34,4 +34,5 @@ public interface PlantInventoryItemRepository extends JpaRepository<PlantInvento
 //    List<PlantInventoryItem> findPlantsNotHiredForPeriod(LocalDate startData,LocalDate endDate);
 
 
+
 }
