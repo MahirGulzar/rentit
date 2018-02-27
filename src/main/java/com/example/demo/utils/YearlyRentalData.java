e400345d6f728d4276973cdd089fe41c1c59e911
mushah <mushah@ttu.ee>
2018-02-27 21:09:51 +0200

    Yearly Rental Data class

--- src/main/java/com/example/demo/utils/YearlyRentalData.java
+++ src/main/java/com/example/demo/utils/YearlyRentalData.java
@@ -1,4 +1,10 @@
 package com.example.demo.utils;
 
+import com.example.demo.models.PlantInventoryEntry;
+
 public class YearlyRentalData {
+    int year;
+    PlantInventoryEntry plant;
+    int numberOfRental;
+    int numberOfCorrectives;
 }
