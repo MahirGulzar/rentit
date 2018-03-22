420c695f73f85298eb0f6480e5532e29952b11ae
junaid ahmed <junahmed46@gmail.com>
2018-03-22 23:44:50 +0200

    initial validators, applied few validation on PO creation.

---
+++ src/main/java/com/example/demo/inventory/application/exceptions/PlantNotFoundException.java
@@ -0,0 +1,14 @@
+package com.example.demo.inventory.application.exceptions;
+
+import com.example.demo.sales.application.dto.PurchaseOrderDTO;
+
+public class PlantNotFoundException extends Throwable {
+
+    private PurchaseOrderDTO purchaseOrder;
+
+    public PlantNotFoundException(String msg, PurchaseOrderDTO purchaseOrder) {
+        super(msg);
+        this.purchaseOrder = purchaseOrder;
+    }
+
+}
