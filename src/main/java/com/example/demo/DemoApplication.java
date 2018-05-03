cf163b71538a10396971b8478b06cd99e962d293
Mahir Gulzar <mahirgulzar@gmail.com>
2018-05-03 06:54:44 +0300

    _Hal Forms for JSON on rentit latest

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -15,62 +15,43 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.hateoas.config.EnableHypermediaSupport;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
-//@EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
+@EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
 public class DemoApplication {
-//
-//	@Configuration
-//	static class ObjectMapperCustomizer {
-//		@Autowired @Qualifier("_halObjectMapper")
-//		private ObjectMapper springHateoasObjectMapper;
-//
-//		@Bean(name = "objectMapper")
-//		ObjectMapper objectMapper() {
-//			return springHateoasObjectMapper
-//					.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
-//					.configure(DeserializationFeature.READ_DATE_TIMESTAMPS_AS_NANOSECONDS, false)
-//					.configure(SerializationFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS, false)
-//					.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
-//					.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
-//					.registerModules(new JavaTimeModule());
-//		}
-//	}
-//
-//	public static void main(String[] args) {
-//		ConfigurableApplicationContext context =SpringApplication.run(DemoApplication.class, args);
-//
-////		PlantInventoryEntry entry = new PlantInventoryEntry();
-////		entry.setName("Bike");
-////		entry.setDescription("Nice and shiny");
-////		entry.setPrice(Money.of(new BigDecimal(100)));
-////
-////
-////		PlantInventoryEntryRepository repo = context.getBean(PlantInventoryEntryRepository.class);
-////		repo.save(entry);
-////
-////
-////		entry = new PlantInventoryEntry();
-////		entry.setName("Truck");
-////		entry.setDescription("A bit rusty");
-////		entry.setPrice(Money.of(new BigDecimal(100)));
-////
-////		repo.save(entry);
-//
-//		// % sign is because SQl wild card
-////		System.out.println(repo.findByNameLike("B%"));
-////
-////		System.out.println(repo.findByNameContaining("ik"));
-//
-//
-//
-//
-//
-//
-//
-//	}
+	@Configuration
+	static class ObjectMapperCustomizer {
+		@Autowired
+		@Qualifier("_halObjectMapper")
+		private ObjectMapper springHateoasObjectMapper;
+
+		@Bean(name = "objectMapper")
+		ObjectMapper objectMapper() {
+			return springHateoasObjectMapper
+					.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+					.configure(DeserializationFeature.READ_DATE_TIMESTAMPS_AS_NANOSECONDS, false)
+					.configure(SerializationFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS, false)
+					.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+					.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
+					.registerModules(new JavaTimeModule());
+		}
+		@Bean
+		public RestTemplate restTemplate() {
+			RestTemplate _restTemplate = new RestTemplate();
+			List<HttpMessageConverter<?>> messageConverters = new ArrayList<>();
+			messageConverters.add(new MappingJackson2HttpMessageConverter(springHateoasObjectMapper));
+			_restTemplate.setMessageConverters(messageConverters);
+			return _restTemplate;
+		}
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
