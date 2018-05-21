aa723ea1dfd305176a15a52d3d91ae38f01911b2
Mahir Gulzar <mahirgulzar@gmail.com>
2018-05-21 22:23:20 +0300

    Project Started...

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -26,3 +26,17 @@
 --     values (2, 2, 'A02');
 -- insert into plant_inventory_item (id, plant_info_id, serial_number)
 --     values (3, 3, 'A03');
+
+
+
+-- Auth Credentials
+
+create table if not exists users (username varchar(50) not null primary key, password varchar(50) not null, enabled boolean not null)
+create table if not exists authorities (username varchar(50) not null, authority varchar(50) not null, constraint FK_AUTHORITIES_USERS foreign key(username) references users(username))
+
+insert into users (username, password, enabled) values ('admin', 'admin', true);
+insert into authorities (username, authority) values ('admin', 'ROLE_ADMIN');
+insert into users (username, password, enabled) values ('employee', 'employee', true);
+insert into authorities (username, authority) values ('employee', 'ROLE_EMPLOYEE');
+insert into users (username, password, enabled) values ('customer', 'customer', true);
+insert into authorities (username, authority) values ('customer', 'ROLE_CUSTOMER');
